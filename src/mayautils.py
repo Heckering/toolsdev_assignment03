5860ca9e61e9e8919f7d142ebf4017b2515a76e6
Blake Wilson <blakewilson1026@gmail.com>
2020-07-29 09:54:06 -0500

    finished segment creator functions

--- src/mayautils.py
+++ src/mayautils.py
@@ -24,7 +24,7 @@ class RoadMakerUtils(object):
     def update_xyz(self, coordinates):
         """called when the user clicks one of the directional arrows.
         updates the current location and previous location"""
-        self.previouslocations[self.segmentnum] = [self.currentxyz]
+        self.previouslocations[self.segmentnum] = self.currentxyz
         self.segmentnum += 1
         self.currentxyz = coordinates
     
@@ -41,21 +41,60 @@ class RoadMakerUtils(object):
         if currentxyz is equal to starting xyz
             create road segment rotated 90 degrees at starting location
         call update xyz"""
+        if self.currentxyz == self.startxyz:
+            maya.cmds.polyCube(name = "RoadSegment" + str(self.segmentnum))
+            startcube = self.find_cube(self.segmentnum)
+            maya.cmds.move(self.startxyz[0], self.startxyz[1], self.currentxyz[2], 
+                           startcube)
+        self.update_xyz([self.currentxyz[0], self.currentxyz[1], self.currentxyz[2] - 1])
+        maya.cmds.polyCube(name = "RoadSegment" + str(self.segmentnum))
+        cube = self.find_cube(self.segmentnum)
+        maya.cmds.move(self.currentxyz[0], self.currentxyz[1], self.currentxyz[2], cube)
 
     def down_segment(self):
         """create a road segment rotated 90 degrees and +1 in Z direction
         if currentxyz is equal to starting xyz
             create road segment rotated 90 degrees at starting location
         call update xyz"""
+        if self.currentxyz == self.startxyz:
+            maya.cmds.polyCube(name = "RoadSegment" + str(self.segmentnum))
+            startcube = self.find_cube(self.segmentnum)
+            maya.cmds.move(self.startxyz[0], self.startxyz[1], self.currentxyz[2], 
+                           startcube)
+        self.update_xyz([self.currentxyz[0], self.currentxyz[1], self.currentxyz[2] + 1])
+        maya.cmds.polyCube(name = "RoadSegment" + str(self.segmentnum))
+        cube = self.find_cube(self.segmentnum)
+        maya.cmds.move(self.currentxyz[0], self.currentxyz[1], self.currentxyz[2], cube)
     
     def right_segment(self):
         """create a road segment +1 in X direction
         if currentxyz is equal to starting xyz
             create road segment at starting location
         call update xyz"""
+        if self.currentxyz == self.startxyz:
+            maya.cmds.polyCube(name = "RoadSegment" + str(self.segmentnum))
+            startcube = self.find_cube(self.segmentnum)
+            maya.cmds.move(self.startxyz[0], self.startxyz[1], self.currentxyz[2], 
+                           startcube)
+        self.update_xyz([self.currentxyz[0] + 1, self.currentxyz[1], self.currentxyz[2]])
+        maya.cmds.polyCube(name = "RoadSegment" + str(self.segmentnum))
+        cube = self.find_cube(self.segmentnum)
+        maya.cmds.move(self.currentxyz[0], self.currentxyz[1], self.currentxyz[2], cube)
 
-    def right_segment(self):
+    def left_segment(self):
         """create a road segment -1 in X direction
         if currentxyz is equal to starting xyz
             create road segment at starting location
         call update xyz"""
+        if self.currentxyz == self.startxyz:
+            maya.cmds.polyCube(name = "RoadSegment" + str(self.segmentnum))
+            startcube = self.find_cube(self.segmentnum)
+            maya.cmds.move(self.startxyz[0], self.startxyz[1], self.currentxyz[2], 
+                           startcube)
+        self.update_xyz([self.currentxyz[0] - 1, self.currentxyz[1], self.currentxyz[2]])
+        maya.cmds.polyCube(name = "RoadSegment" + str(self.segmentnum))
+        cube = self.find_cube(self.segmentnum)
+        maya.cmds.move(self.currentxyz[0], self.currentxyz[1], self.currentxyz[2], cube)
+
+    def find_cube(self, segmentnum):
+        return maya.cmds.ls('RoadSegment' + str(segmentnum))
